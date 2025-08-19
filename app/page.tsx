7fe95035bdf2dd0e2b0d60f39b50e1f143c2b80b
charu1603 <kakdechaitrali16@gmail.com>
2025-08-19 15:56:21 +0530

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-screen flex ">
+      <div className="w-[30vw] min-h-screen"></div>
+      <div className="w-[70vw] min-h-screen border-l-2"></div>
+    </div>
+  );
+}
